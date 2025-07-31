bab341ecb682b53a8fdcde55fe46012db4020b19
Itsuki0503 <itsuki2053@gmail.com>
2025-07-31 21:57:31 +0900

    starting rust

---
+++ horror_escape_game/target/debug/deps/horror_escape_game.d
@@ -0,0 +1,5 @@
+E:\games\horror_game\horror_escape_game\target\debug\deps\horror_escape_game.d: src\main.rs
+
+E:\games\horror_game\horror_escape_game\target\debug\deps\horror_escape_game.exe: src\main.rs
+
+src\main.rs:
---
+++ horror_escape_game/target/debug/horror_escape_game.d
@@ -0,0 +1,1 @@
+E:\games\horror_game\horror_escape_game\target\debug\horror_escape_game.exe: E:\games\horror_game\horror_escape_game\src\main.rs
